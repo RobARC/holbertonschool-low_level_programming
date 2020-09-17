c2fa61e5547e766726e7cb3ab5443ddce087c287
RobARC <rrondonc@gmail.com>
2020-09-16 19:37:54 -0500

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+/**
+* This is program to hello puts
+*
+*/
+#include<stdio.h>
+int main(void)/*This is the main function*/
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);/*This is the end*/
+}
