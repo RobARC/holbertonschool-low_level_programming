a3183e2e07302e3cf7e6a68904414d1974ed2560
RobARC <rrondonc@gmail.com>
2020-09-16 21:44:44 -0500

    Hello Priintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>/*
 *This program is about printf function
-*
+*Function main ok
 *Return: Always 0 (Succes)
 *
 */
-int main(void) /**This is the main function**/
+int main(void) /*Funtion main*/
 {
-      printf("with proper grammar, but the outcome is a piece of art,\n");
-      return (0);/*End*/
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);/*End*/
 }
