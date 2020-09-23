a2328fbd3812c46f21ba3ba5c70080f5ca18a184
RobARC <rrondonc@gmail.com>
2020-09-23 17:31:14 -0500

    Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,9 +4,9 @@
 * @c: Is an entry
 * Return:  Always 0 (Success)
 */
-int _isalpha(int c) 
+int _isalpha(int c)
 {
-if  (c > 96  && c < 123 || c > 64  && c < 91) 
+if ((c > 96  && c < 123) || (c > 64  && c < 91))
 c = 1;
 else
 c = 0;
