0ee79c0057b182e019eb04945ce24cc34b833a03
RobARC <rrondonc@gmail.com>
2020-09-23 16:58:28 -0500

    IsAlpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _isalpha -Entry point
+* @c: Is an entry
+* Return:  Always 0 (Success)
+*/
+int _isalpha(int c) 
+{
+if  (c > 96  && c < 123 || c > 64  && c < 91) 
+c = 1;
+else
+c = 0;
+return (c);
+}
