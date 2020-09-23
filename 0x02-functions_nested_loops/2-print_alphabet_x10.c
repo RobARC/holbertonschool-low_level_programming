c20c888970b9742c42f0c973dca5eebec74c186a
RobARC <rrondonc@gmail.com>
2020-09-23 09:12:08 -0500

    Alphabet x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "holberton.h"
+/**
+* print_alphabet_x10 -Entry point
+*
+* Return Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+int n;
+int j;
+
+for (j = 0; j <= 10; j++)
+{
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+
+
+}
+}
