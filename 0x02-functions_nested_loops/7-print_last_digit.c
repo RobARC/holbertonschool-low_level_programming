493a88f8be7c48ac51520706a1ef9ffa42049025
RobARC <rrondonc@gmail.com>
2020-09-23 21:44:09 -0500

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,17 +10,19 @@ if (a > 0)
 {
 a = (a  % 10);
 a = (a * 1);
+_putchar(a + '0');
 return (a);
 }
 else if  (a < 0)
 {
 a = (a % 10);
 a = (a * -1);
+_putchar(a + '0');
 return (a);
 }
 else
 {
+_putchar(a + '0');
 return (a);
 }
-
 }
