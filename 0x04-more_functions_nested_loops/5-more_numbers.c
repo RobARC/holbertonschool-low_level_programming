193b12d273280e0990e5406748dfdf0d1d01de54
RobARC <rrondonc@gmail.com>
2020-09-24 19:41:09 -0500

    More Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* print_alphabet_x10 -Entry point
+* more_numbers -Entry point
 *
 * Return Always 0 (Success)
 */
@@ -10,19 +10,17 @@ void more_numbers(void)
 int n;
 int j;
 
-
 for (j = 0; j <= 9; j++)
 {
-
-        for (n = 0; n <= 14; n++)
-        {
-		if ( n > 9)
-		{
-		  _putchar ((n/10)+'0');
-		}
-          _putchar((n%10)+'0'); 
-        }
-        _putchar('\n');
+for (n = 0; n <= 14; n++)
+{
+if (n > 9)
+{
+_putchar ((n / 10) + '0');
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
 }
 }
 
