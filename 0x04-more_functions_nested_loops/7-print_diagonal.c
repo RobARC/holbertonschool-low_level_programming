99ee213afc239def615ce914b75d54c3882b2bd9
RobARC <rrondonc@gmail.com>
2020-09-24 22:52:19 -0500

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* print_line -Entry point
+* @n: is a variable
+* Return Always 0 (Success)
+*/
+void print_diagonal(int n)
+{
+int j;
+int i;
+char d = '\\';
+char e = 32;
+for (j = 1; j <= n; j++)
+{
+	for (i = 1; i <= j; i++)
+	{
+		_putchar(e);
+		
+	}
+_putchar(d);
+_putchar('\n');
+}
+_putchar('\n'); 
+}
+
