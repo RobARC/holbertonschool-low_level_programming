729f29f092b36544976cd798315276d5bb39f7a7
RobARC <rrondonc@gmail.com>
2020-09-27 09:14:32 -0500

    print triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+/**
+* print_triangle-Entry point
+* @size: is a variable
+* Return Always 0 (Success)
+*/
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+int esp = size;
+
+	if (size > 0)
+	{
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= esp; j++)
+		{
+			_putchar(32);
+
+		}
+		for (k = 1; k <= i; k++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+		esp = esp - 1;
+
+	}
+
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
+
+
+
+
+
