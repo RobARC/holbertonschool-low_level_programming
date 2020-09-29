b5bf8f01f956dad4b9bf003abccc81df626e3639
RobARC <rrondonc@gmail.com>
2020-09-28 20:26:48 -0500

    Project 0x05

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+* reset_to_98-Entry point
+* @n: is a pointer
+* Return Always 0 (Success)
+*/
+void reset_to_98(int *n)
+{
+int x;
+x = 98;
+*n = x;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,13 @@
+#This is the file README to the project 0x05-pointers_arrays_strings
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
