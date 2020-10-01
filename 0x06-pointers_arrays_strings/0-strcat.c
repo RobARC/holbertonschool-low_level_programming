9e4f0b72155e1c1911e26d3f683bc4280a762a55
RobARC <rrondonc@gmail.com>
2020-10-01 15:55:43 -0500

    function srtcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * *_strcat-Entry point
+ * @dest: is a variable pointer
+ * @src: is a variable pointer
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
 int a;
 int b;
-	for (a =0; dest[a] != '\0'; a++)
+	for (a = 0; dest[a] != '\0'; a++)
 	{
 	}
-	for(b = 0; src[b] != '\0'; b++)
+	for (b = 0; src[b] != '\0'; b++)
 	{
 		dest[a++] = src[b];
 	}
