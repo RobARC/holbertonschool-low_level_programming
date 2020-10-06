be3f7821af6939feb45ba423d82dde3cfb8793da
RobARC <rrondonc@gmail.com>
2020-10-05 19:12:58 -0500

    find a caracter a return the rest of string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+ * *_strchr-Entry point
+ * @c: is a char variable
+ * @s: is a variable pointer
+ * @
+ *  Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+
+		for (i = 0; *s != '\0'; i++)
+		{
+			if (s[i] == c)
+			{
+				break;
+			}
+		}
+		return (&s[i]);
+	return (NULL);
+}
