5694dbeca8be41f7ec452b671072d5aa3918ba60
RobARC <rrondonc@gmail.com>
2020-10-06 18:08:32 -0500

    function strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * *_strstr-Entry point
+ * @haystack: is a variable pointer
+ * @needle: is a variable pointer
+ * @
+ *  Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int j;
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (haystack[j] == needle[i])
+			{
+				return (&haystack[j]);
+			}
+		}
+
+	}
+	return (NULL);
+}
