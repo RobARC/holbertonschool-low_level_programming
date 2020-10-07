f0893c2af3b6972a778c76f53a41a951310f64e9
RobARC <rrondonc@gmail.com>
2020-10-07 13:51:33 -0500

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _puts_recursion-Entry point
+ * @s: is a variable pointer
+ * @
+ * @
+ *  Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+
+}
