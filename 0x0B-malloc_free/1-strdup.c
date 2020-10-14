2a087b55f20bceb8b7bd5a667175e0509d83c3ed
RobARC <rrondonc@gmail.com>
2020-10-13 21:20:41 -0500

    uploading task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include<stdlib.h>
+/**
+ * _strdup-Entry point
+ * @
+ * @
+ * @str: is a pointer variable char
+ *  Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+char *len;
+unsigned int i, l;
+
+		for (i = 0; str[i] != 0; i++)
+		{}
+		if (str == NULL)
+			return (NULL);
+
+		len = (char *) malloc((i + 1) * sizeof(char));
+
+		if (len == NULL)
+			return (NULL);
+
+		for (l = 0; str[l] != '\0'; l++)
+
+			len[l] = str[l];
+			return (len);
+
+}
+
