110a44101e1ee91654c0de59339cdb1db083e9c4
RobARC <rrondonc@gmail.com>
2020-10-13 21:29:05 -0500

    updating task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,12 +12,12 @@ char *_strdup(char *str)
 char *len;
 unsigned int i, l;
 
-		for (i = 0; str[i] != 0; i++)
+		for (i = 0; str[i] != '\0'; i++)
 		{}
 		if (str == NULL)
 			return (NULL);
 
-		len = (char *) malloc(i * sizeof(char));
+		len = (char *) malloc((i + 1) * sizeof(char));
 
 		if (len == NULL)
 			return (NULL);
