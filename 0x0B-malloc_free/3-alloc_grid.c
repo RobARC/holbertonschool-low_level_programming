b00342b5643e0e33f0cbf82b9d33d87050dc3487
RobARC <rrondonc@gmail.com>
2020-10-14 20:18:01 -0500

    uploading task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "holberton.h"
+/**
+ *  alloc_grid-Entry point
+ * @width: is variable int
+ * @height: is a variable
+ *  Return: Always 0.
+ */
+int **alloc_grid(int width, int height)
+{
+
+	int **grid;
+	int i;
+	int j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+		for (j = 0; j <  width; j++)
+			grid[i][j] = 0;
+
+	}
+
+	return (grid);
+	free(grid);
+}
