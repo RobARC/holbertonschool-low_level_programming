ceb41edaaf26b71b2e4568e5209933bfd8ac0e67
RobARC <rrondonc@gmail.com>
2020-10-15 19:06:43 -0500

    uplodaing task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,68 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include "holberton.h"
+
+/**
+ *  _strlen-Entry point
+ *
+ * @s: is a pointer variable char
+ *  Return: Always 0.
+ */
+int  _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; s++)
+
+	i++;
+	return (i);
+}
+/**
+ *  string_nconcat-Entry point
+ * @n: is variable unsigned int
+ * @s1: is a pointer variable
+ * @s2: is a pointer variable
+ *  Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j;
+unsigned int l1, l2;
+char *p;
+
+
+	if (s1 == NULL)
+		s1 = "";
+	 l1 = _strlen(s1);
+	if (s2 == NULL)
+		s2 = "";
+	 l2 = _strlen(s2);
+
+	if (n >= l2)
+		l2 = l2 + 1; /*nota: en este momento todo el  largo de s2*/
+				/* es l2 = l2 +1 deberia dar 11*/
+
+	p = (char *)malloc((l1 + l2 + 1) * sizeof(char));
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] =  s1[i];
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+
+
+		if (j == n)
+		{
+			return (p);
+		}
+		else
+		{
+			p[i + j] = s2[j];
+		}
+
+	return (NULL);
+}
