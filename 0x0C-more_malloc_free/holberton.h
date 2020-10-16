a7b1a79f43288d0bec6334bb68544f4cce6641de
RobARC <rrondonc@gmail.com>
2020-10-16 08:03:18 -0500

    updating holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -64,8 +64,11 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
-void *malloc_checked(unsigned int b);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
 
 
 
