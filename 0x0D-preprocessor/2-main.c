3e70bbd34ae90ce08900203d2541ede2f35e83d4
RobARC <rrondonc@gmail.com>
2020-10-19 14:41:08 -0500

    iploading task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* main-Entry point print a nname of a file
+* @
+* @
+* Return: Always 0
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
