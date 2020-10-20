b0ff61c4a9640fbda98dcef6fa8a6be7237d436a
RobARC <rrondonc@gmail.com>
2020-10-19 21:04:04 -0500

    uploading project 0x0E

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,15 @@
+# This is the RAEDME fiel about proyect 0x0E. C - Structures, typedef
+# Will be learn about strutures of data types and how it works.
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG
+#define DOG
+
+/**
+* struct dog -dog data
+* @name: is a variable char name of the dog
+* @age: is a float variable age of the dog
+* @owner: is a char variable owner the dog
+*/
+
+struct dog
+	{
+	char *name;
+	float age;
+	char *owner;
+	} dog;
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif
