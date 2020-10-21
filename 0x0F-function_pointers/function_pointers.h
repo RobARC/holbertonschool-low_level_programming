6033fff296ca35a3cfe00679362da902a775a41f
RobARC <rrondonc@gmail.com>
2020-10-21 14:15:50 -0500

    updating function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,7 @@
 #ifndef FUNCTION_TO_POINTER_H
 #define FUNCTION_TO_POINTER_H
-/**
-* FUNCTION_TO_POINTER_H is our header file
-* to the proyect 0x0F-function_pointers
-* @name: pointer to the string cointaining the name
-*
-*/
-int _putchar(char c);
+
+int _putchar (char c);
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
