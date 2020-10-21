31cc82b213796e2ef09b59d8612d4b5aec6687eb
RobARC <rrondonc@gmail.com>
2020-10-21 14:20:40 -0500

    uploading task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator- use a pointer to a function to print na$
+ * @array: pointer to the array containing the elem.
+ * @size:  size of the array
+ * @action: pointer to the functions
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
