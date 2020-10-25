bd5d0394e5c756c0951637af92510492cc0f7c5d
RobARC <rrondonc@gmail.com>
2020-10-25 16:08:52 -0500

    uploading task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,53 +2,49 @@
 #include<stdlib.h>
 #include<stdarg.h>
 #include "variadic_functions.h"
-
 /**
 * print_all - add the value of the list of parameters
-*
 * @format: arguments
-*
 */
 void print_all(const char * const format, ...)
 {
 va_list list;
 int i = 0;
-int j = 0;
 char *ptr;
-
-	va_start(list, format);
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		j = 0;
-		switch (format[i])
+		va_start(list, format);
+		while (format[i] != '\0')
 		{
-		case 'c':
-			printf("%c", va_arg(list, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(list, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(list, double));
-			break;
-		case 's':
-			ptr = va_arg(list, char *);
-			if (ptr == NULL)
+			switch (format[i])
 			{
-				printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(list, int));
 				break;
+			case 'i':
+				printf("%d", va_arg(list, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double));
+				break;
+			case 's':
+				ptr = va_arg(list, char *);
+				if (ptr == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", ptr);
+				break;
+			default:
+			i++;
+			continue;
 			}
-			printf("%s", ptr);
-			break;
-		default:
-			j = 1;
-			break;
+			if (format[i + 1] != '\0')
+				printf(", ");
+			i++;
 		}
-		if (format[i + 1] != '\0' && j == 0)
-			printf(", ");
-		i++;
 	}
 	va_end(list);
 	printf("\n");
 }
-
