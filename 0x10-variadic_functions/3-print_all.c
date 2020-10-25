317ba7d8c318e7ad9edf3e4094a8773f0fd0098d
RobARC <rrondonc@gmail.com>
2020-10-25 15:13:00 -0500

    uploading task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include "variadic_functions.h"
+
+/**
+* print_all - add the value of the list of parameters
+*
+* @format: arguments
+*
+*/
+void print_all(const char * const format, ...)
+{
+va_list list;
+int i = 0;
+int j = 0;
+char *ptr;
+
+	va_start(list, format);
+	while (format[i] != '\0')
+	{
+		j = 0;
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(list, double));
+			break;
+		case 's':
+			ptr = va_arg(list, char *);
+			if (ptr == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", ptr);
+			break;
+		default:
+			j = 1;
+			break;
+		}
+		if (format[i + 1] != '\0' && j == 0)
+			printf(", ");
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
+
