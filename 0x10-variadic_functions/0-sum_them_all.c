cef2a92dae45a4f2cd4915ea26e0b6b3a521a19a
RobARC <rrondonc@gmail.com>
2020-10-25 07:03:57 -0500

    updating task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,23 +15,23 @@ int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i, sum;
 
-if (n == 0)
-{
-	return (0);
-}
-else
-{
-	va_list list;
-	va_start(list, n);
-	sum = 0;
-	for(i = 0; i < n; i++)
+	if (n == 0)
 	{
-		sum += va_arg(list, unsigned int);
+		return (0);
 	}
-	va_end(list);
-	return (sum);
-}
+	else
+	{
+		va_list list;
+		va_start(list, n);
+		sum = 0;
+		for(i = 0; i < n; i++)
+		{
+			sum += va_arg(list, unsigned int);
+		}
+		va_end(list);
+		return (sum);
+	}
 
-return (0);
+	return (0);
 
 }
