f844751eac9c057c311b9c10f484353f28adfd6e
RobARC <rrondonc@gmail.com>
2020-11-02 16:10:09 -0500

    Uploading task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *functionBeforeMain - Function Before Main
+ *
+ *
+ */
+
+
+void functionBeforeMain (void)
+{
+
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+
+}
+
+
