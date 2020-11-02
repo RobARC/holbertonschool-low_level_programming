a4627dfcff975d1d93c9920c23b93df7a3f42dc7
RobARC <rrondonc@gmail.com>
2020-11-02 16:19:31 -0500

    Updating task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,11 +8,11 @@
  */
 
 
-void functionBeforeMain (void)
+void functionBeforeMain(void)
 {
 
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
 
