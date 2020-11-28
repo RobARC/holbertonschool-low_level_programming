ad3eab74168260aaea6b08ef2e06ea416b6d9812
RobARC <rrondonc@gmail.com>
2020-11-28 11:45:44 +0000

    updating task hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
 * Return: Always 0 (Success)
 */
 
-int main(void)/*This is the main function*/
+int main(void)
 {
 puts(\"Programming is like building a multilingual puzzle");
-return (0);/*This is the end*/
+return (0);
 }
