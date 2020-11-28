5f52e7cdf58da5b1cf109282cd7f0c67011bb908
RobARC <rrondonc@gmail.com>
2020-11-28 18:52:43 +0000

    uploading _putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,7 @@
 #include "holberton.h"
 
+void print_number_recursive(unsigned int n);
+
 /**
  * print_number - Prints an integer.
  * @n: Integer to be printed.
@@ -20,5 +22,23 @@ void print_number(int n)
 	{
 		i = n;
 	}
+	print_number_recursive(i);
 }
+/**
+ * print_number_recursive - Recursive function to print integers.
+ * @n: unsigned integer.
+ *
+ * Return: void.
+ */
+
+void print_number_recursive(unsigned int n)
+{
+	if (n / 10 != 0)
+		print_number_recursive(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+
+
+
 
