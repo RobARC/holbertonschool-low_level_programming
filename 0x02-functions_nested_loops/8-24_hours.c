e1bf2814cf18105635ade969a0f499f9c531d957
RobARC <rrondonc@gmail.com>
2020-11-28 14:56:27 +0000

    uploading task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from
+ * 00:00 to 23:59
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minutes;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
