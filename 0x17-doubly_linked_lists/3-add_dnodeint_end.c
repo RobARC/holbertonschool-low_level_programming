20b22ccb1fb121a4c1884f4376ca292b7c079704
Roberth <rrondoc@gmail.com>
2020-12-13 10:49:14 -0500

    Updating file 3-add_dnodeint_end.c betty style

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,33 +1,32 @@
 #include "lists.h"
 
 /**
- * dlistint_len - function print elements in double linked lists
- * @h: pointer to the head double linked lists
- * @count: variable counter
+ * add_dnodeint_end - function print elements in double linked lists
+ * @head: pointer to the head double linked lists
+ * @n:the value data node will contain
  * Return: the size the double linked list
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 
 	dlistint_t *tmp; /* pointer to create new nodo */
-	dlistint_t *tmp2; /* pointer to link the last nodo */	
+	dlistint_t *tmp2; /* pointer to link the last nodo */
 
 	if (head == NULL) /* ask for head is NULL */
 	{
-		return(NULL);
+		return (NULL);
 	}
-	
 
 	tmp =  malloc(sizeof(dlistint_t)); /* create memory space to struct node */
-    	if (tmp == NULL)
-    	{
-        	return (NULL);
-     	}	
+	if (tmp == NULL)
+	{
+		return (NULL);
+	}
 
-    	tmp->n = n;
-    	tmp->next = NULL;
-    	tmp->prev = NULL;
-    	
+	tmp->n = n;
+	tmp->next = NULL;
+	tmp->prev = NULL;
+
 	if (*head != NULL) /* first time *head it will be NULL */
 	{
 		tmp2 = *head;
@@ -43,5 +42,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = tmp; /* to create head the first time */
 	}
 
-	return(tmp);
+	return (tmp);
 }
