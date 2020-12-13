845a1a8e1dc9598f6f17f8c9146e36ea7f596d2c
Roberth <rrondoc@gmail.com>
2020-12-13 17:40:32 -0500

    Uploading task 7 insert a node in given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index-function insert a new node at a given position.
+ *@h: Pointer to node head
+ *@idx: Variable Index
+ *@n: Node Data
+ *Return: New node insert
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int count = 0;
+	dlistint_t *new, *aft;
+
+	aft = *h;
+
+	if (h == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	while (aft != NULL)
+	{
+		if (count == idx) /* find the position to insert */
+			break;
+		count++;
+		aft = aft->next;
+	}
+	if (count != idx)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = aft->next; /* pointer to aft->next */
+	new->prev = aft->prev; /* pointer to aft-prev */
+
+	if (aft->next != NULL) /* if find the last nodo jump this step */
+		(aft->next)->prev = new; /* pointer prev to new */
+	aft->next = new;
+
+	return (new);
+
+}
