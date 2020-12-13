e55cc66e587545679ac11b535892df8b5a8ff656
Roberth <rrondoc@gmail.com>
2020-12-13 09:48:28 -0500

    Updating file 0-print_dlistint.c betty style

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,6 @@
 /**
  * print_dlistint - function print elements in double linked lists
  * @h: pointer to the head double linked lists
- * @count: variable counter
  * Return: the size the double linked list
  */
 size_t print_dlistint(const dlistint_t *h)
