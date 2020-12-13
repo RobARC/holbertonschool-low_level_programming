583490bb3e4b0cf924ae9a3143c3aa59b3d4434b
Roberth <rrondoc@gmail.com>
2020-12-13 11:10:37 -0500

    Updating file 4-get_dnedeint.c betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,8 +1,8 @@
 #include "lists.h"
-/**get_dnodeint_at_index get the node index at the doble linked list
+/**
+ *get_dnodeint_at_index - get the node index at the doble linked list
  *@head: pointer to the head at the double linked list
  *@index: index give to find
- *@count: counter to compare with index
  *Return: the node give
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
@@ -10,15 +10,15 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 unsigned int count = 0;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 
 	while (head != NULL)
 	{
 		if (count == index)
-			return(head);
+			return (head);
 		count++;
 		head = head->next;
 	}
-	return(NULL);
+	return (NULL);
 
 }
