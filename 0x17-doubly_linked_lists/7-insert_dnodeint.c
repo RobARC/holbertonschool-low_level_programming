e9f814a98f3259b2376f1abd1c0dcbae5d8aa16e
Roberth <rrondoc@gmail.com>
2020-12-14 09:23:31 -0500

    Updating task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,45 +7,64 @@
  *Return: New node insert
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
-{	unsigned int count = 0;
-	dlistint_t *new, *aft;
+{	unsigned int count = 0, len = 0;
+	dlistint_t *newnode = NULL, *tmpnode = *h;
 
-	aft = *h;
+	len = dlistint_len(*h);
 
-	if (h == NULL && idx != 0)
-		return (NULL);
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{	free(new);
-		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
-	if (idx == 0) /* is the given position is the first*/
-	{	new->next = *h;
-		*h = new;
-		aft->prev = new;
-		return (new);
-	}
-	while (aft != NULL)
+	if (idx <= len)
 	{
-		if (count == idx - 1) /* find the position to insert */
-			break;
-		count++;
-		aft = aft->next;
+		if (h == NULL && idx != 0)
+			return (NULL);
+		if (idx == 0) /* is the given position is the first*/
+			newnode = add_dnodeint(h, n);
+		else if (idx == len)
+			newnode = add_dnodeint_end(h, n);
+		else
+		{
+			newnode = malloc(sizeof(dlistint_t));
+			if (newnode == NULL)
+			{
+				free(newnode);
+				return (NULL);
+			}
+			newnode->n = n;
+			while (tmpnode != NULL)
+			{
+				if (count == idx - 1) /* find the position to insert */
+				{
+					newnode->next = tmpnode->next;
+					(tmpnode->next)->prev = newnode;
+					tmpnode->next = newnode;
+					newnode->prev = tmpnode;
+					return (newnode);
+				}
+				count++;
+				tmpnode = tmpnode->next;
+			}
+		}
+		return (newnode);
 	}
-	if (count != idx - 1)
-	{	free(new);
-		return (NULL);
-	}
-	new->prev = aft; /* pointer to aft-prev */
-	new->next = aft->next; /* pointer to aft->next */
-	aft->next = new;
-	aft = aft->next;
-	aft = aft->next;
-	if (aft != NULL) /* if find the last nodo jump this step */
-		aft->prev = new; /* pointer prev to new */
-
-	return (new);
+	return (NULL);
 }
+
+/**
+* dlistint_len - count all elements at the double linked
+* @h: pointer to head double linked list
+* Return: the number of nodes at the double linked list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nodes = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
+
