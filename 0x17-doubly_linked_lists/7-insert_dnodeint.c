8b0dc941458a3318cec4202470db938ac503f8b6
Roberth <rrondoc@gmail.com>
2020-12-13 20:39:07 -0500

    Updating task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = NULL;
 	new->prev = NULL;
 
+	if (idx == 0) /* is the given position is the first */
+	{
+		new->next = *h;
+		*h = new;
+		aft->prev = new;
+		return (new);
+	}
+		
 	while (aft != NULL)
 	{
 		if (count == idx) /* find the position to insert */
