e87709154c769f409978ff8d4d35af98573a6d48
Roberth <rrondoc@gmail.com>
2021-01-15 19:37:49 -0500

    updating task 0 adding calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *HT = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *HT;
+       
+	if (size <= 0)
+		return (NULL);
+	
+	HT = calloc(1, sizeof(hash_table_t));
 
 	if (HT == NULL)
 		return (NULL);
 
 	HT->size = size;
 	HT->array = calloc(size, sizeof(hash_node_t *));
-
+	if (HT->array == NULL)
+	{
+		free(HT);
+		return(NULL);
+	}
 	return (HT);
 }
