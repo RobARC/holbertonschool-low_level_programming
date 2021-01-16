ac4aa5d92d47a878ba33775e760dc87cedfafd9a
Roberth <rrondoc@gmail.com>
2021-01-15 19:42:04 -0500

    updating task 0 betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,10 +12,10 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *HT;
-       
+
 	if (size <= 0)
 		return (NULL);
-	
+
 	HT = calloc(1, sizeof(hash_table_t));
 
 	if (HT == NULL)
@@ -26,7 +26,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (HT->array == NULL)
 	{
 		free(HT);
-		return(NULL);
+		return (NULL);
 	}
 	return (HT);
 }
